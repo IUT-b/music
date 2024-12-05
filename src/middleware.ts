50b625e5f52c6d6c5236e30f9da2e5f562a1bf4f
bteoutlook <bte@outlook.jp>
2024-12-06 05:23:22 +0900

    ミドルウェアを設定

---
+++ src/middleware.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt';
+
+export async function middleware(req: Request) {
+    // セッション（JWTトークン）を取得
+    const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+    // トークンがない場合（認証されていない場合）
+    if (!token) {
+        return NextResponse.redirect(new URL('/', req.url));
+    }
+
+    return NextResponse.next();
+}
+
+// middlewareが適用されるURLパターンを指定
+export const config = {
+    matcher: ['/insights', '/favorites', '/playlists', '/schedule'],
+};
